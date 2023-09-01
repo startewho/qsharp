81221b831cea1cee4492be5cfe7343283277bdf9
startewho <hoolooday@live.com>
2023-09-01 13:04:06 +0800

    添加QuickJS模块

---
+++ quickjs/build.rs
@@ -0,0 +1,38 @@
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    bindgen::Builder::default()
+        .header("quickjspp/quickjs-libc.h")
+        .header("quickjspp/quickjs.h")
+        
+        .generate()
+        .unwrap()
+        .write_to_file("src/quickjs.rs")
+        .unwrap();
+
+    // using cc, build and link c code
+    cc::Build::new()
+        .files([
+            "quickjspp/cutils.c",
+            "quickjspp/libbf.c",
+            "quickjspp/libregexp.c",
+            "quickjspp/libunicode.c",
+            "quickjspp/quickjs-libc.c",
+            "quickjspp/quickjs-libc.c",
+            "quickjspp/quickjs.c",
+        ])
+        .define("CONFIG_BIGNUM",None)
+        .define("CONFIG_DEBUGGER", None)
+        .compile("quickjs");
+
+    // csbindgen code, generate both rust ffi and C# dll import
+    csbindgen::Builder::default()
+        .input_bindgen_file("src/quickjs.rs") // read from bindgen generated code
+        .rust_file_header("use super::quickjs::*;") // import bindgen generated modules(struct/method)
+        .csharp_dll_name("quickjs")
+        .csharp_namespace("QuickJs") 
+        .csharp_entry_point_prefix("csbindgen_")
+        .generate_to_file("src/quickjs_ffi.rs", "../NativeMethods.quickjs.g.cs")
+        .unwrap();
+    Ok(())
+}
---
+++ quickjs/src/lib.rs
@@ -0,0 +1,11 @@
+use std::{
+    collections::HashSet,
+    ffi::{c_char, c_long, c_ulong, CString},
+};
+
+#[allow(dead_code)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+#[allow(non_upper_case_globals)]
+mod quickjs;
+mod quickjs_ffi;    
